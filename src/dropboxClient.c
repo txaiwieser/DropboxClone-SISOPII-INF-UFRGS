dc9c354bd92693efb9cc9a503ff636010138b566
Augusto Bennemann <gutobenn@gmail.com>
2017-04-20 18:55:40 -0300

    close_connection()

--- src/dropboxClient.c
+++ src/dropboxClient.c
@@ -9,122 +9,121 @@
 char server_host[50]; // TODO max host name length?
 int server_port = 0;
 
-void cmdUpload(){
-        printf("-> UPLOAD\n");
+void cmdUpload() {
+    printf("-> UPLOAD\n");
 };
 
-void cmdDownload(){
-        //char *download_test_method = "DOWNLOAD filename.ext\0"; // TODO fazer funcionar sem esse \0 ou inclui-lo na hora de enviar pro servidor?
-        printf("-> DOWNLOAD\n");
+void cmdDownload() {
+    //char *download_test_method = "DOWNLOAD filename.ext\0"; // TODO fazer funcionar sem esse \0 ou inclui-lo na hora de enviar pro servidor?
+    printf("-> DOWNLOAD\n");
 };
 
-void cmdList(){
-        char *method = "LIST";
-        int valread;
-        char buffer[1024] = {0};
+void cmdList() {
+    char * method = "LIST";
+    int valread;
+    char buffer[1024] = {
+        0
+    };
 
-        int sock = connect_server(server_host, server_port);
-        debug_printf("[Socket number %d]\n", sock);
-        // TODO verificar valor do sock se é > 0?
+    int sock = connect_server(server_host, server_port);
+    debug_printf("[Socket number %d]\n", sock);
+    // TODO verificar valor do sock se é > 0?
 
-        send(sock, method, strlen(method), 0 );
-        debug_printf("[%s method sent]\n", method);
-        valread = read( sock, buffer, 1024);
-        printf("Files: %s\n", buffer );
-        // TODO close_connection();
+    send(sock, method, strlen(method), 0);
+    debug_printf("[%s method sent]\n", method);
+    valread = read(sock, buffer, 1024);
+    printf("Files: \n%s\n", buffer);
+    close_connection(sock);
 };
 
-void cmdGetSyncDir(){
+void cmdGetSyncDir() {};
+
+void cmdMan() {
+    printf("\nAvailable commands:\n");
+    printf("get_sync_dir\n");
+    printf("list\n");
+    printf("download <filename.ext>\n");
+    printf("upload <path/filename.ext>\n");
+    printf("help\n\n");
+    printf("exit\n");
 };
 
-void cmdMan(){
-        printf("\nAvailable commands:\n");
-        printf("get_sync_dir\n");
-        printf("list\n");
-        printf("download <filename.ext>\n");
-        printf("upload <path/filename.ext>\n");
-        printf("help\n\n");
-        printf("exit\n");
-};
+int main(int argc, char * argv[]) {
+    char cmd[256];
+    char * token;
 
-int main(int argc, char *argv[]){
-        char cmd[256];
-        char *token;
+    if (argc < 4) { // Número incorreto de parametros
+        printf("Usage: %s <user> <IP> <port>\n", argv[0]);
+        return 1;
+    }
+    debug_printf("[Client started with parameters User=%s IP=%s Port=%s]\n", argv[1], argv[2], argv[3]);
+    strcpy(server_host, argv[2]);
+    server_port = atoi(argv[3]);
 
-        if (argc < 4) { // Número incorreto de parametros
-                printf("Usage: %s <user> <IP> <port>\n", argv[0]);
-                return 1;
+    printf("Welcome to Dropbox! - v 1.0\n");
+    cmdMan();
+
+    while (1) {
+        printf("Dropbox> ");
+        gets(cmd);
+        if ((token = strtok(cmd, " \t")) != NULL) {
+            if (strcmp(token, "exit") == 0) {
+                break;
+            } else if (strcmp(token, "upload") == 0) cmdUpload();
+            else if (strcmp(token, "download") == 0) cmdDownload();
+            else if (strcmp(token, "list") == 0) cmdList();
+            else if (strcmp(token, "get_sync_dir") == 0) cmdGetSyncDir();
+            else if (strcmp(token, "help") == 0) cmdMan();
+            else printf("Invalid command! Type 'help' to see the available commands\n");
         }
-        debug_printf("[Client started with parameters User=%s IP=%s Port=%s]\n", argv[1], argv[2], argv[3]);
-        strcpy(server_host, argv[2]);
-        server_port = atoi(argv[3]);
+    }
 
-        printf ("Welcome to Dropbox! - v 1.0\n");
-        cmdMan();
-
-        while (1) {
-                printf ("Dropbox> ");
-                gets(cmd);
-                if( (token = strtok(cmd," \t")) != NULL ) {
-                        if (strcmp(token,"exit")==0) {
-                                break;
-                        }
-                        else if (strcmp(token,"upload")==0) cmdUpload();
-                        else if (strcmp(token,"download")==0) cmdDownload();
-                        else if (strcmp(token,"list")==0) cmdList();
-                        else if (strcmp(token,"get_sync_dir")==0) cmdGetSyncDir();
-                        else if (strcmp(token,"help")==0) cmdMan();
-                        else printf ("Invalid command! Type 'help' to see the available commands\n");
-                }
-        }
-
-        return 0;
+    return 0;
 }
 
-int connect_server(char *host, int port){
-        struct sockaddr_in address;
-        int sock = 0;
-        struct sockaddr_in serv_addr;
+int connect_server(char * host, int port) {
+    struct sockaddr_in address;
+    int sock = 0;
+    struct sockaddr_in serv_addr;
 
-        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        {
-                printf("\n Socket creation error \n");
-                return -1;
-        }
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-        memset(&serv_addr, '0', sizeof(serv_addr));
+    memset( & serv_addr, '0', sizeof(serv_addr));
 
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(port);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
 
-        // Convert IPv4 and IPv6 addresses from text to binary form
-        if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-        {
-                printf("\nInvalid address/ Address not supported \n");
-                return -1;
-        }
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", & serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        {
-                printf("\nConnection Failed \n");
-                return -1;
-        }
+    if (connect(sock, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-        return sock;
+    return sock;
 }
 
-void sync_client(){
+void sync_client() {
 
 }
 
-void send_file(char *file){
+void send_file(char * file) {
 
 }
 
-void get_file(char *file){
+void get_file(char * file) {
 
 }
 
-void close_connection(){
+void close_connection(int sock) {
+  shutdown(sock, 2); // Stop both reception and transmission.
 
+  // TODO esse parametro pode ser utilizado? na especificação do trabalho, em "resumo da interface de implementação minima para o cliente" essa funcao aparece sem nenhum parametro. mas como está dito "minima" e me parece necessário passar pelo menos o identificador do socket pra poder encerrar a conexão, adicionei como parametro
 }
--- src/dropboxServer.c
+++ src/dropboxServer.c
@@ -6,87 +6,78 @@
 #include "../include/dropboxUtil.h"
 #include "../include/dropboxServer.h"
 
-#define PORT 3003
-
-int main(){
-								printf("Server started\n");
-
-								int server_fd, new_socket, valread;
-								struct sockaddr_in address;
-								int opt = 1;
-								int addrlen = sizeof(address);
-								char buffer[1024] = {0};
-								char *list_of_files = "filename1.ext\nfilename2.ext\nfilename3.ext\n";
-
-// Creating socket file descriptor
-								if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-								{
-																perror("socket failed");
-																exit(EXIT_FAILURE);
-								}
-
-// Forcefully attaching socket to the port 8080
-								if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-																							&opt, sizeof(opt)))
-								{
-																perror("setsockopt");
-																exit(EXIT_FAILURE);
-								}
-								address.sin_family = AF_INET;
-								address.sin_addr.s_addr = INADDR_ANY;
-								address.sin_port = htons( PORT );
-
-// Forcefully attaching socket to the port 8080
-								if (bind(server_fd, (struct sockaddr *)&address,
-																	sizeof(address))<0)
-								{
-																perror("bind failed");
-																exit(EXIT_FAILURE);
-								}
-								if (listen(server_fd, 3) < 0)
-								{
-																perror("listen");
-																exit(EXIT_FAILURE);
-								}
-								if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-																																	(socklen_t*)&addrlen))<0)
-								{
-																perror("accept");
-																exit(EXIT_FAILURE);
-								}
-								valread = read( new_socket, buffer, 1024);
-
-								if( !strncmp(buffer, "LIST", 4) ) {
-																printf("Request method: LIST\n");
-								}
-								else if( !strncmp(buffer, "GET_SYNC_DIR", 12) ) {
-																printf("Request method: GET_SYNC_DIR\n");
-								}
-								else if( !strncmp(buffer, "DOWNLOAD", 8) ) {
-																printf("Request method: DOWNLOAD\n");
-																printf("Filename: %s\n", buffer+8);
-								}
-								else if( !strncmp(buffer, "UPLOAD", 6) ) {
-																printf("Request method: UPLOAD\n");
-																// TODO get filename
-								};
-
-								printf("BUFFER: %s\n",buffer );
-								send(new_socket, list_of_files, strlen(list_of_files), 0 );
-								printf("List of files sent\n");
+#define PORT 3003 // TODO definir porta por parametro, assim como é feito no client?
 
 
-								return 0;
+int main() {
+    printf("Server started\n");
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char * list_of_files = "filename1.ext\nfilename2.ext\nfilename3.ext\n";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, & opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr * ) & address,
+            sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr * ) & address,
+            (socklen_t * ) & addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+
+    if (!strncmp(buffer, "LIST", 4)) {
+        printf("Request method: LIST\n");
+    } else if (!strncmp(buffer, "GET_SYNC_DIR", 12)) {
+        printf("Request method: GET_SYNC_DIR\n");
+    } else if (!strncmp(buffer, "DOWNLOAD", 8)) {
+        printf("Request method: DOWNLOAD\n");
+        printf("Filename: %s\n", buffer + 8);
+    } else if (!strncmp(buffer, "UPLOAD", 6)) {
+        printf("Request method: UPLOAD\n");
+        // TODO get filename
+    };
+
+    printf("BUFFER: %s\n", buffer);
+    send(new_socket, list_of_files, strlen(list_of_files), 0);
+    printf("List of files sent\n");
+
+    return 0;
 }
 
-void sync_server(){
+void sync_server() {
 
 }
 
-void receive_file(char *file){
+void receive_file(char * file) {
 
 }
 
-void send_file(char *file){
+void send_file(char * file) {
 
 }
